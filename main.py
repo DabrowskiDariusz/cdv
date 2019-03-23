7e999c079b2142b0667876ef5bc494c092639102
ddabrow1 <dariusz.dabrowski93@gmail.com>
2019-03-23 13:52:02 +0100

    Pierwszy program - ciag dalszy

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 #pierwszy program
 print("CDV2019")
+print(2)
+
+#potega
+potega = 2 ** 10
+print(potega)
+
+tekst = "CDV"
+print(tekst * 2)
+
+#pobieranie danych z klawiatury
+imie = input()
+print("Twoje imie podane z klawiatury: " + imie)
+
+nazwisko = 'Nowak'
+print("Twoje dane: " + imie + " " + nazwisko)
+
+dlugosc = len(nazwisko)
+print(dlugosc)
+
+print(type(imie))
+print(type(nazwisko))
+
+wiek = input()
+print("Wiek: " + wiek)
+
+#wiekAla = 30
+#wiekAla = str(wiekAla) #int converted to str
+#print(type(wiekAla))
+#print("Wiek Ali: " + wiekAla)
+
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+
+ostatniZnak = nazwisko[len(nazwisko) -1]
+print(ostatniZnak)
+
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+
+#konwersja
+x = "5" * 1
+print(type(x))
+x = int(x)
+print(type(x))
+
+y = 4
+print(type(y))
+y = 4 / 2
+print(type(y))
+print(y)
